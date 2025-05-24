d6bf758fc80f1da660245a008fa855c6be4d532f
Adira Rana <58012161+AlphaAdira@users.noreply.github.com>
2025-05-23 18:44:55 -0700

    Add files via upload

---
+++ src/Scenes/Tutorial.js
@@ -0,0 +1,252 @@
+class Tutorial extends Phaser.Scene {
+    constructor() {
+        super("tutorialScene");
+        this.myHealth = 100;
+        this.index = 0;
+    }
+
+    init() {
+        // variables and settings
+        this.ACCELERATION = 200;
+        this.DRAG = 700;    // DRAG < ACCELERATION = icy slide
+        this.physics.world.gravity.y = 1500;
+        this.JUMP_VELOCITY = -600;
+        this.PARTICLE_VELOCITY = 50;
+    }
+
+    preload(){
+        this.load.scenePlugin('AnimatedTiles', './lib/AnimatedTiles.js', 'animatedTiles', 'animatedTiles');
+    }
+    create() {
+        // Create a new tilemap game object which uses 18x18 pixel tiles, and is
+        // 100 tiles wide and 50 tiles tall.
+        this.map = this.add.tilemap("platformer-level-1", 18, 18, 100, 50);
+
+        // Add a tileset to the map
+        // First parameter: name we gave the tileset in Tiled
+        // Second parameter: key for the tilesheet (from this.load.image in Load.js)
+        this.tileset = this.map.addTilesetImage("tilemap_packed", "tilemap_tiles");
+
+        // Create a layer
+        this.pipesLayer1 = this.map.createLayer("pipes1", this.tileset, 0, 0);
+        this.pipesLayer2 = this.map.createLayer("pipes2", this.tileset, 0, 0);
+        this.deathLayer = this.map.createLayer("acid", this.tileset, 0, 0);
+        this.groundLayer = this.map.createLayer("platformT", this.tileset, 0, 0);
+
+        // Make it collidable - platforms
+        this.groundLayer.setCollisionByProperty({
+            collides: true
+        });
+
+        this.walkSound = this.sound.add("walkAudio", {
+            volume: .3,
+            detune: 1200
+        });
+        this.wait = this.sound.add("walkAudio", {
+            volume: 0
+        });
+        ////////////////////
+        // Particles!!!
+        ////////////////////
+        this.vfx = {};
+
+        this.vfx.walking = this.add.particles(0, 0, "kenny-particles", {
+            frame: ['dirt_01.png'],
+            scale: {start: 0.01, end: 0.05},
+            lifespan: 500,
+            alpha: {start: 1, end: 0.1}, 
+        });
+        this.vfx.walking.stop();
+
+        this.vfx.hop = this.add.particles(0, 0, "kenny-particles", {
+            frame: ['dirt_03.png'],
+            scale: {start: 0.05, end: 0.15},
+            lifespan: 500,
+            alpha: {start: 1, end: 0.1}, 
+        });
+        this.vfx.hop.stop();
+
+        // set up player avatar
+        my.sprite.player = this.physics.add.sprite(500, 650, "platformer_characters", "tile_0000.png")
+        my.sprite.player.setCollideWorldBounds(true);
+
+        // Enable collision handling
+        this.physics.add.collider(my.sprite.player, this.groundLayer);
+        
+
+        // set up Phaser-provided cursor key input
+        cursors = this.input.keyboard.createCursorKeys();
+
+        this.cameras.main.setBounds(0, 0, 1800, 900);
+        this.cameras.main.startFollow(my.sprite.player, true, 0.25, 0.25); // (target, [,roundPixels][,lerpX][,lerpY])
+        this.cameras.main.setDeadzone(0, 0);
+        this.cameras.main.setZoom(4);
+
+        /////////////
+        // SCORE TEXT
+        ////////////
+        my.text.health = this.add.text(20, 350, this.myHealth, {
+            font: 'bold 15px "Comic Sans MS", sans-serif',
+            fill: 'red',
+            stroke: '#ffffff', // Outline color (black)
+            strokeThickness: 2, // Thickness of the outline
+            wordWrap: {
+                width: 1800
+            }
+        });
+
+        this.next = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+        my.text.help = this.add.text(my.sprite.player.x - 210, 620, "Use arrow keys to move", {
+            font: 'bold 30px "Comic Sans MS", sans-serif',
+            fill: 'black',
+            stroke: '#ffffff', // Outline color (black)
+            strokeThickness: 2, // Thickness of the outline
+            wordWrap: {
+                width: 1800
+            }
+        });
+        my.text.spaceNext = this.add.text(my.sprite.player.x - 210, 670, "[Space] to go to next rule", {
+            font: 'bold 15px "Comic Sans MS", sans-serif',
+            fill: 'black',
+            stroke: '#ffffff', // Outline color (black)
+            strokeThickness: 2, // Thickness of the outline
+            wordWrap: {
+                width: 1800
+            }
+        });
+
+        this.animatedTiles.init(this.map);
+    }
+
+    update() {
+        if(cursors.left.isDown) {
+            my.sprite.player.body.setAccelerationX(-this.ACCELERATION);
+            my.sprite.player.resetFlip();
+            my.sprite.player.anims.play('walk', true);
+            if(my.sprite.player.body.blocked.down){
+                if (!this.walkSound.isPlaying){
+                    this.walkSound.play();
+                    this.myHealth-=2;
+                    my.text.health.setText(this.myHealth);
+                }
+            }
+            
+            this.vfx.walking.startFollow(my.sprite.player, my.sprite.player.displayWidth/2-10, my.sprite.player.displayHeight/2-5, false);
+            this.vfx.walking.setParticleSpeed(this.PARTICLE_VELOCITY, 0);
+            // Only play smoke effect if touching the ground
+            if (my.sprite.player.body.blocked.down) {
+                this.vfx.walking.start();
+            } else {
+                this.vfx.walking.stop();
+            }
+            
+
+        } else if(cursors.right.isDown) {
+            my.sprite.player.body.setAccelerationX(this.ACCELERATION);
+            my.sprite.player.setFlip(true, false);
+            my.sprite.player.anims.play('walk', true);
+            if(my.sprite.player.body.blocked.down){
+                if (!this.walkSound.isPlaying){
+                    this.walkSound.play();
+                    this.myHealth-=2;
+                    my.text.health.setText(this.myHealth);
+                }
+            }
+            
+            this.vfx.walking.startFollow(my.sprite.player, my.sprite.player.displayWidth/2-10, my.sprite.player.displayHeight/2-5, false);
+            this.vfx.walking.setParticleSpeed(this.PARTICLE_VELOCITY, 0);
+            // Only play smoke effect if touching the ground
+            if (my.sprite.player.body.blocked.down) {
+                this.vfx.walking.start();
+            } else {
+                this.vfx.walking.stop();
+            }
+            
+
+        } else {
+            my.sprite.player.body.setAccelerationX(0);
+            my.sprite.player.body.setDragX(this.DRAG);
+            my.sprite.player.anims.play('idle');
+            this.walkSound.pause();
+            this.vfx.walking.stop();
+            if (my.sprite.player.body.blocked.down) {
+                if (!this.wait.isPlaying){
+                    this.wait.play();
+                    if (this.myHealth < 100){
+                        this.myHealth+=1;
+                        my.text.health.setText(this.myHealth);
+                    }
+                }
+            }
+        }
+        
+        let onLadder = false;
+        // player jump
+        // note that we need body.blocked rather than body.touching b/c the former applies to tilemap tiles and the latter to the "ground"
+        if (!onLadder){
+            if(!my.sprite.player.body.blocked.down) {
+                my.sprite.player.anims.play('jump');
+                this.vfx.hop.stop();
+            }
+            if(my.sprite.player.body.blocked.down && Phaser.Input.Keyboard.JustDown(cursors.up)) {
+                my.sprite.player.body.setVelocityY(this.JUMP_VELOCITY);
+                this.sound.play("climbAudio", {
+                    volume: .5,
+                    detune: -1200
+                });
+
+                this.vfx.hop.startFollow(my.sprite.player, my.sprite.player.displayWidth/2-10, my.sprite.player.displayHeight/2-5, false);
+                this.vfx.hop.setParticleSpeed(this.PARTICLE_VELOCITY, 0);
+                // Only play smoke effect if touching the ground
+                if (my.sprite.player.body.blocked.down) {
+                    this.vfx.hop.start();
+                    this.myHealth-=5;
+                    my.text.health.setText(this.myHealth);
+                }
+            }
+        }
+
+        //health
+        my.text.health.x = my.sprite.player.x - 10;
+        my.text.health.y = my.sprite.player.y - 30;
+
+        if (this.myHealth <= 0){
+            this.myHealth = 100;
+            this.scene.start("tutorialScene");
+        }
+
+        if (Phaser.Input.Keyboard.JustDown(this.next)) {
+            score = 0;
+            my.sprite.player.x = 500;
+            my.sprite.player.y = 650;
+            my.text.help.x = my.sprite.player.x - 210;
+            my.text.spaceNext.x = my.sprite.player.x - 210;
+            if (this.index == 0){
+                my.text.help.setText("See the number above your head?");
+                this.index++;
+            } else if (this.index == 1){
+                my.text.help.setText("That's your energy bar");
+                this.index++;
+                console.log(this.index);
+            } else if (this.index == 2){
+                my.text.help.setText("If it hits 0, you loose");
+                this.index++;
+                console.log(this.index);
+            } else if (this.index == 3){
+                my.text.help.setText("Collect tokens to regain energy");
+                this.index++;
+                console.log(this.index);
+            } else if (this.index == 4){
+                my.text.help.setText("You also loose if you fall into acid");
+                this.index++;
+                console.log(this.index);
+            } else if (this.index == 5){
+                my.text.help.setText("Good Luck!");
+                this.index++;
+                console.log(this.index);
+            } else {
+                this.scene.start("platformerScene");
+            }
+        }
+    }
+}
